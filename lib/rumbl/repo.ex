687c91eff29250297f4073a667f5f27ce8ab8c73
Riza Fahmi <rizafahmi@gmail.com>
2016-09-11 19:56:29 +0700

    Finish dummy Repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,30 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  # use Ecto.Repo, otp_app: :rumbl
+
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Riza Fahmi",
+    username: "riza", password: "elixir"},
+    %Rumbl.User{id: "2", name: "Haaris Ramadhan",
+    username: "haaris", password: "phoenix"}
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map ->
+      map.id == id
+    end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} ->
+      Map.get(map, key) == val end)
+    end
+  end
 end
